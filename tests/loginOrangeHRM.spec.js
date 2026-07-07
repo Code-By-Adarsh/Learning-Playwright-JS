52eb62c13277ad83ff8f7a50fc22a04b4686d6cb
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-07-07 17:27:51 +0530

    Added valid logout test to loginOrangeHRM

--- tests/loginOrangeHRM.spec.js
+++ tests/loginOrangeHRM.spec.js
@@ -1,16 +1,24 @@
+//@ts-check
 import {test,expect} from '@playwright/test'
 
 test("Valid login",async ({page})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-
     //getBy
     await page.getByPlaceholder("Username").fill("Admin")
-
     //by css locator
     await page.locator("input[name='password']").fill("admin123")
-
     //by xpath
     await page.locator("//button[@type='submit']").click()
+    await expect(page).toHaveURL(/dashboard/)
+})
 
-    await expect(page.url().includes("dashboard")).toBeTruthy()
+test("Valid logout", async ({page})=>{
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    await page.getByPlaceholder("Username").fill("Admin")
+    await page.locator("input[name='password']").fill("admin123")
+    await page.locator("//button[@type='submit']").click()
+    await expect(page).toHaveURL(/dashboard/)
+    await page.getByAltText("profile picture").first().click()
+    await page.getByText("Logout").click()
+    await expect(page).toHaveURL(/login/)
 })
