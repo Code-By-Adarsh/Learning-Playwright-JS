b3906e7c4e4ba1bc83ae7e04bcd40c01efbbd01b
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-07-13 11:00:43 +0530

    Playwright configuration done

--- playwright.config.js
+++ playwright.config.js
@@ -14,24 +14,37 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   timeout:60000,
+  globalTimeout:10*60*100,
+
+  // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: './tests',
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  
+  /* Retry 2 times on CI and 1 times on local*/
+  retries: process.env.CI ? 2 : 1,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    screenshot:'on',
+    video:'on-first-retry',
     trace: 'on-first-retry',
+    viewport:{width:922,height:411},
+    headless:true,
   },
 
   /* Configure projects for major browsers */
@@ -40,10 +53,12 @@ export default defineConfig({
       name: 'chromium',
       use: { 
         ...devices['Desktop Chrome'],
+        /*
         viewport:{width:922,height:411},
         screenshot:"on", //by default -> off
         video:"on", //by default -> off
         trace:"on" //by default -> off
+        */
       },
     },
 
