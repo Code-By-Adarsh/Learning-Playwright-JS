5af27dc28bdb250f6f9a06fa46c4882b49509a02
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-07-26 22:32:38 +0530

    Assertion practice done

---
+++ tests/practiceAssertion.spec.js
@@ -0,0 +1,27 @@
+//@ts-check
+import {test,expect} from '@playwright/test'
+
+test.describe("Basic Functionality Testing",()=>{
+    test.beforeEach("Open page",async ({page})=>{
+        await page.goto('https://practicesoftwaretesting.com/')
+    })
+
+    test("Verify Sign In Link Present",async ({page})=>{
+        await expect(page.locator('[data-test="nav-sign-in"]')).toHaveText('Sign in')
+    })
+
+    test("Verify Title", async ({page})=>{
+        await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0")
+    })
+
+    test("Verify counting of items displayed",async ({page})=>{
+        const productGrid = page.locator(".col-md-9")
+        await expect(productGrid.getByRole("link")).toHaveCount(9)
+    })
+
+    test("Verify searching functionality",async ({page})=>{
+        await page.locator('[data-test="search-query"]').fill("Thor Hammer")
+        await page.locator('[data-test="search-submit"]').click()
+        await expect(page.getByAltText("Thor Hammer")).toBeVisible()
+    })
+})
