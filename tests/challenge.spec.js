5bfd9975b37c34b42d898e7849d0476976ee54cd
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-08-02 21:57:18 +0530

    Challenge 2 - Comppleted

--- tests/challenge.spec.js
+++ tests/challenge.spec.js
@@ -18,12 +18,19 @@ test.describe("Challenge Accepted",()=>{
         await page.locator('[data-test="proceed-1"]').click();
         await page.locator('[data-test="proceed-2"]').click();await page.locator('[data-test="postal_code"]').fill('0000');
         await page.locator('[data-test="house_number"]').fill('103');
-        await expect.soft(page.locator('[data-test="proceed-3"]')).toBeEnabled({timeout:10000})
+        await expect.soft(page.locator('[data-test="proceed-3"]')).toBeEnabled({timeout:20000})
         await page.locator('[data-test="proceed-3"]').click();
         await page.locator('[data-test="payment-method"]').selectOption('cash-on-delivery');
         await page.locator('[data-test="finish"]').click();
         await expect.soft(page.locator('[data-test="payment-success-message"]')).toContainText('Payment was successful');
         await page.locator('[data-test="finish"]').click();
         await expect(page.locator('#order-confirmation')).toHaveText(/Thanks for your order!/);
+    })
+
+    test("Challenge 2 - Visual Test",async ({page})=>{
+        await page.locator('[data-test="nav-home"]').click();
+        await page.getByAltText("Combination Pliers").click();
+        await expect(page.locator('[data-test="product-name"]')).toBeVisible();
+        await expect(page).toHaveScreenshot("Combination-Pliers.png")
     })
 })
