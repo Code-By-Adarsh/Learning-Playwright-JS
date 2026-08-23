2f2a7045ffd4b5cce9af45c6bc2ea8beb4f0bbd9
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-08-23 18:57:08 +0530

    Implemented fixture with pom

--- tests/pomLearning/login.spec.js
+++ tests/pomLearning/login.spec.js
@@ -1,8 +1,14 @@
-import {test,expect} from '@playwright/test'
+import {test as base,expect} from '@playwright/test'
 import { LoginPage } from '../../pages/loginPage'
 
-test("Verify valid login",async({page})=>{
-    const loginPage = new LoginPage(page)
+const test = base.extend({
+    loginPage:async({page}, use)=>{
+        const loginPage = new LoginPage(page)
+        await use(loginPage)
+    }
+})
+
+test("Verify valid login",async({page,loginPage})=>{
     await loginPage.goTo()
     await loginPage.login('test@gmail.com','test123')
     await expect(page).toHaveURL(/test12/)
